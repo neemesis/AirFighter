29f3b788717af53932e815b5b2fbbccc6d1310f4
Mirche Toshevski <toshevski@live.de>
2015-05-04 17:36:56 +0200

    Dodadena klasa SpaceShip i Player

---
+++ AirFighter/Player.cs
@@ -0,0 +1,23 @@
+﻿using AirFighter.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirFighter {
+    public class Player : SpaceShip {
+
+        public Player() : base (new Point(580, 200), 100, Resources.plane2) {
+        }
+
+        public override void RemoveHealth() {
+            Health -= 10;
+        }
+
+        public override void Shot() {
+            
+        }
+    }
+}
---
+++ AirFighter/SpaceShip.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirFighter {
+    public abstract class SpaceShip {
+        public Point Position { get; private set; }
+        public int Health { get; protected set; }
+        protected Image ShipImage;
+
+        public SpaceShip() {
+            Position = Point.Empty;
+        }
+
+        public SpaceShip(Point Position, int Health, Bitmap ShipImage) {
+            this.Position = Position;
+            this.Health = Health;
+            this.ShipImage = ShipImage;
+        }
+
+        public void SetPosition(Point Position) {
+            this.Position = Position;
+        }
+
+        public void Draw(Graphics g) {
+            g.DrawImage(ShipImage, Position);
+        }
+
+        public abstract void RemoveHealth();
+        
+        public abstract void Shot();
+    }
+}
